6229ff9f0fbeab4bc1f86dfc75252a4a88a51018
Slingshotng <dmobolade@gmail.com>
2022-06-22 12:05:58 -0600

    intro to js

---
+++ script.js
@@ -0,0 +1,11 @@
+let fullname = "Name = David Mobolade, ";
+let height = "Height = 150cm, ";
+let country = "Country = Nigeria";
+
+
+
+
+document.write(fullname);
+
+document.write(height);
+document.write(country);
